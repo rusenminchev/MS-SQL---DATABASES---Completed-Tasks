4c2ca553b49263fc89afca97154ce077672a9dba
Rusen Minchev <rusenminchev@gmail.com>
2021-04-27 21:42:33 +0300

    add

---
+++ 06. Indices and Data Aggregation/07. Deposits Filter.sql
@@ -0,0 +1,7 @@
+SELECT w.DepositGroup,
+		SUM(w.DepositAmount) AS [TotalSum]
+FROM WizzardDeposits w
+WHERE w.MagicWandCreator = 'Ollivander family'
+GROUP BY w.DepositGroup
+HAVING SUM(w.DepositAmount) < 150000
+ORDER BY TotalSum DESC
